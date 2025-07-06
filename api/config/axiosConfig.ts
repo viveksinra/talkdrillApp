17818aafc381e65de87380ec885ddfb186b31b33
Hrishikesh Baidya <hrishikesh@Hrishikeshs-MacBook-Air.local>
2025-07-07 00:39:42 +0530

    initial wallet

--- api/config/axiosConfig.ts
+++ api/config/axiosConfig.ts
@@ -1,7 +1,7 @@
 import * as SecureStore from 'expo-secure-store';
 
 // TODO: Change to api.talkdrill.com when ready
-const baseUrl = "https://api.talkdrill.com";
+const baseUrl = "https://b47b-103-215-226-35.ngrok-free.app" //"https://api.talkdrill.com";
 
 // Use ngrok URL for development, but be sure to update when URL changes
 export const API_BASE_URL = baseUrl;
